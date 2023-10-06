1c5ff4d61b1a732b684555c5532f4f4dd98b985f
Kyrylo Simonov <xi@resolvent.net>
2023-10-06 12:58:00 -0500

    Support for catalogs in connect_with_funsql()

--- src/general.jl
+++ src/general.jl
@@ -42,30 +42,26 @@ function connect_to_databricks(; catalog = nothing, schema = nothing)
     DBInterface.connect(ODBC.Connection, DATABRICKS_DSN)
 end
 
-function connect_with_funsql(conn::ODBC.Connection, cols)
-    cat = FunSQL.SQLCatalog(
-        tables = FunSQL.tables_from_column_list(cols),
-        dialect = FunSQL.SQLDialect(:spark))
-
-    FunSQL.SQLConnection(conn, catalog = cat)
-end
+isuppercaseor_(s) =
+    all(ch -> isuppercase(ch) || ch == '_', s)
 
 function connect_with_funsql(specs...; catalog = nothing, exclude = nothing)
-    DATABRICKS_CATALOG = ENV["DATABRICKS_CATALOG"]
+    DATABRICKS_CATALOG = get(ENV, "DATABRICKS_CATALOG", "ctsi")
     catalog = something(catalog, DATABRICKS_CATALOG)
 
     conn = connect_to_databricks(catalog = catalog)
     table_map = Dict{Symbol, FunSQL.SQLTable}()
     for spec in specs
-        prefix, schemaname = _unpack_spec(spec)
+        prefix, (catalogname, schemaname) = _unpack_spec(spec, catalog)
         raw_cols = ODBC.columns(conn,
-                                catalogname = catalog,
+                                catalogname = catalogname,
                                 schemaname = schemaname)
-        cols = [(lowercase(row.TABLE_SCHEM),
+        cols = [(lowercase(row.TABLE_CAT),
+                 lowercase(row.TABLE_SCHEM),
                  lowercase(row.TABLE_NAME),
-                 lowercase(row.COLUMN_NAME))
+                 isuppercaseor_(row.COLUMN_NAME) ? lowercase(row.COLUMN_NAME) : row.COLUMN_NAME)
                 for row in Tables.rows(raw_cols)]
-        tables = FunSQL.tables_from_column_list(cols)
+        tables = _tables_from_column_list(cols)
         for table in tables
             exclude === nothing || !exclude(table) || continue
             name = Symbol("$prefix$(table.name)")
@@ -77,11 +73,53 @@ function connect_with_funsql(specs...; catalog = nothing, exclude = nothing)
     FunSQL.SQLConnection(conn, catalog = cat)
 end
 
-_unpack_spec(schema::Union{Symbol, AbstractString}) =
-    "", string(schema)
+_unpack_spec(schema::Union{Symbol, AbstractString, NTuple{2, Union{Symbol, AbstractString}}}, default_catalog) =
+    "", _split_schema(schema, default_catalog)
 
-_unpack_spec(pair::Pair) =
-    string(first(pair)), string(last(pair))
+_unpack_spec(pair::Pair, default_catalog) =
+    string(first(pair)), _split_schema(last(pair), default_catalog)
+
+function _split_schema(schema::AbstractString, default_catalog)
+    parts = split(schema, '.', limit = 2)
+    length(parts) == 1 ?
+      (default_catalog, string(parts[1])) : (string(parts[1]), string(parts[2]))
+end
+
+_split_schema(schema::NTuple{2, Union{Symbol, AbstractString}}, default_catalog) =
+    string(schema[1]), string(schema[2])
+
+function _tables_from_column_list(rows)
+    tables = FunSQL.SQLTable[]
+    qualifiers = Symbol[]
+    catalog = schema = name = nothing
+    columns = Symbol[]
+    for (cat, s, n, c) in rows
+        cat = Symbol(cat)
+        s = Symbol(s)
+        n = Symbol(n)
+        c = Symbol(c)
+        if cat === catalog && s === schema && n === name
+            push!(columns, c)
+        else
+            if !isempty(columns)
+                t = FunSQL.SQLTable(qualifiers = qualifiers, name = name, columns = columns)
+                push!(tables, t)
+            end
+            if cat !== catalog || s !== schema
+                qualifiers = [cat, s]
+            end
+            catalog = cat
+            schema = s
+            name = n
+            columns = [c]
+        end
+    end
+    if !isempty(columns)
+        t = FunSQL.SQLTable(qualifiers = qualifiers, name = name, columns = columns)
+        push!(tables, t)
+    end
+    tables
+end
 
 function cursor_to_dataframe(cr)
     df = DataFrame(cr)
