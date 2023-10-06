ad178a6b1b5acc9ae46a436cf94f9802a0887a58
Kyrylo Simonov <xi@resolvent.net>
2023-10-06 13:13:48 -0500

    Export Spark and Databricks functions and operators

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,13 +1,6 @@
 @funsql begin
 
-datediff_day(a, b) = `datediff(DAY, ?, ?)`($a, $b)
-datediff_year(a, b) = `datediff(YEAR, ?, ?)`($a, $b)
 is_date_between(d, s, f) = `(? BETWEEN ? AND ?)`($d, $s, $f)
-
-ilike(s, pat) = ` ILIKE `($s, $pat)
-
-ilike(s, pats...) =
-	or($([@funsql(ilike($s, $pat)) for pat in pats]...))
 
 like_acronym(s, pat) =
 	$(' ' in pat ? @funsql(` ILIKE `($s, $("%$(pat)%"))) :
