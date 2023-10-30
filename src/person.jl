eeef38ab40cc828c62590ffacb077b5199ae0685
Clark C. Evans <cce@clarkevans.com>
2023-10-30 12:35:20 -0400

    improving roundup support for person demographics

--- src/person.jl
+++ src/person.jl
@@ -34,13 +34,13 @@ join_by_person(next::FunSQL.SQLNode; carry=[]) = begin
     define($([@funsql($n => base.$n) for n in carry]...))
 end
 
-count_n_person() = begin
-    define(n_person => roundup(count()))
+count_n_person(; roundup=true) = begin
+    define(n_person => count())
     order(n_person.desc())
-    define(n_person => concat("≤", n_person))
+    define(n_person => roundups(n_person, $roundup))
 end
 
-stratify_by_age() = begin
+stratify_by_age(; roundup=true) = begin
     join(p => from(person), p.person_id == person_id)
     define(age => 2023 - p.year_of_birth)
     group(age => case(
@@ -51,24 +51,24 @@ stratify_by_age() = begin
         age >= 40, "40-49",
         age >= 30, "30-39",
         "29 or less"))
-    count_n_person()
+    count_n_person(; roundup=$roundup)
     order(age)
     select(n_person, age)
 end
 
-stratify_by_race() = begin
+stratify_by_race(; roundup=true) = begin
     join(p => person(), p.person_id == person_id)
     filter(p.race_concept_id > 0 )
     group(race => p.race.concept_name)
-    count_n_person()
+    count_n_person(; roundup=$roundup)
     select(n_person, race)
 end
 
-stratify_by_ethnicity() = begin
+stratify_by_ethnicity(; roundup=true) = begin
     join(p => person(), p.person_id == person_id)
     filter(p.ethnicity_concept_id > 0 )
     group(ethnicity => p.ethnicity.concept_name)
-    count_n_person()
+    count_n_person(; roundup=$roundup)
     select(n_person, ethnicity)
 end
 
