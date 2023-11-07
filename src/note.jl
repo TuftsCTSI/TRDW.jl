fd2516db499e0d7e6c2ae9ac27871c84a128dea6
kzollove <kzollovenecek@tuftsmedicalcenter.org>
2023-11-07 11:23:58 -0500

    Add join_note_class_via_cohort

--- src/note.jl
+++ src/note.jl
@@ -4,4 +4,15 @@ note() = begin
     from(note)
 end
 
+join_note_class_via_cohort(match...; exclude=nothing) = begin
+    join_via_cohort(
+        $(@funsql begin
+            from(note)
+            select(note_id, person_id, note_date, note_class_concept_id)
+        end), note_date; match_prefix=note_class, match=$match)
+    $(isnothing(exclude) ? @funsql(define()) :
+      @funsql(filter(!concept_matches($exclude; match_prefix=note_class))))
+    define(concept_id => note_class_concept_id)
+end
+
 end
