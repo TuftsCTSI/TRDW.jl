912416ebe37007a240a186f12562320cb83f791f
Clark C. Evans <cce@clarkevans.com>
2023-12-30 09:35:29 -0500

    making stratify functions aware of index() pairs

---
+++ src/counting.jl
@@ -0,0 +1,80 @@
+@funsql begin
+
+count_n_person(; roundup=true) = begin
+    define(n_person => count_distinct(person_id))
+    order(n_person.desc())
+    define(n_person => roundups(n_person, $roundup))
+end
+
+count_n_person(pair::Pair{Symbol, FunSQL.SQLNode}; roundup=true) =
+    $(pair[2]).count_n_person(; roundup=true)
+
+cohort_count(; roundup=true) = begin
+    group()
+    count_n_person(;roundup=$roundup)
+    define(n_event => count(person_id))
+    define(n_event => roundups(n_event, $roundup))
+end
+
+cohort_count(pair::Pair{Symbol, FunSQL.SQLNode}; roundup=true) =
+    $(pair[2]).cohort_count(; roundup=true)
+
+stratify_by_age(; roundup=true) = begin
+    deduplicate(person_id)
+    as(base)
+    join(person(), base.person_id == person_id)
+    define(age => current_age())
+    group(age => case(
+        age >= 90, "90+",
+        age >= 80, "80-89",
+        age >= 70, "70-79",
+        age >= 70, "70-79",
+        age >= 60, "60-69",
+        age >= 50, "50-59",
+        age >= 40, "40-49",
+        age >= 30, "30-39",
+        age >= 20, "20-29",
+        "19 or less"))
+    count_n_person(; roundup=$roundup)
+    order(age)
+    select(n_person, age)
+end
+
+stratify_by_age(pair::Pair{Symbol, FunSQL.SQLNode}; roundup=true) =
+    $(pair[2]).stratify_by_age(; roundup=true)
+
+stratify_by_race(; roundup=true) = begin
+    deduplicate(person_id)
+    as(base)
+    join(from(person), base.person_id == person_id)
+    left_join(race => from(concept),
+        race_concept_id == race.concept_id)
+    define(race_name =>
+        race_concept_id == 0 ?
+        "Unspecified" : race.concept_name)
+    group(race_name)
+    count_n_person(; roundup=$roundup)
+    select(n_person, race_name)
+end
+
+stratify_by_race(pair::Pair{Symbol, FunSQL.SQLNode}; roundup=true) =
+    $(pair[2]).stratify_by_race(; roundup=true)
+
+stratify_by_ethnicity(; roundup=true) = begin
+    deduplicate(person_id)
+    as(base)
+    join(from(person), base.person_id == person_id)
+    left_join(ethnicity => from(concept),
+        ethnicity_concept_id == ethnicity.concept_id)
+    define(ethnicity_name =>
+        ethnicity_concept_id == 0 ?
+        "Unspecified" : ethnicity.concept_name)
+    group(ethnicity_name)
+    count_n_person(; roundup=$roundup)
+    select(n_person, ethnicity_name)
+end
+
+stratify_by_ethnicity(pair::Pair{Symbol, FunSQL.SQLNode}; roundup=true) =
+    $(pair[2]).stratify_by_ethnicity(; roundup=true)
+
+end
