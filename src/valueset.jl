ba50114103c367549f340100bfdb4cb136be9212
Clark C. Evans <cce@clarkevans.com>
2024-02-02 11:13:23 -0500

    turn the smoking behavior valueset into a function to avoid problems with precompilation of g_vocab_conn

--- src/valueset.jl
+++ src/valueset.jl
@@ -1,4 +1,4 @@
-smoking_behavior_concepts = [
+smoking_behavior_concepts() = [
 	    OMOP_Extension("OMOP5181846","Cigar smoker"),
         OMOP_Extension("OMOP5181838","Cigarette smoker"),
         OMOP_Extension("OMOP5181836","Electronic cigarette smoker"),
@@ -6,7 +6,7 @@ smoking_behavior_concepts = [
         OMOP_Extension("OMOP5181837","Passive smoker"),
         OMOP_Extension("OMOP5181845","Pipe smoker")]
 
-@funsql smoking_behavior_concepts() = concept($smoking_behavior_concepts...)
+@funsql smoking_behavior_concepts() = concept($(smoking_behavior_concepts())...)
 
 @funsql matches_smoking_behavior() =
-    concept_matches($smoking_behavior_concepts; match_on=value_as)
+    concept_matches($(smoking_behavior_concepts()); match_on=value_as)
