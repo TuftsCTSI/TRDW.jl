7301bf0a19c5ba641964f5474b6eeb05e8515ecb
Clark C. Evans <cce@clarkevans.com>
2024-03-31 15:31:58 -0400

    include IRB in template block; format subtitle

--- src/template.jl
+++ src/template.jl
@@ -12,7 +12,7 @@ const wide_notebook_style = html"""
 WideStyle() =
     wide_notebook_style
 
-NotebookFooter(;case=nothing, sfid=nothing, pims=nothing) = @htl("""
+NotebookFooter(;case=nothing, sfid=nothing) = @htl("""
   <div>
     <table style="width: 100%">
     <tr><td style="width: 72; vertical-align: top">
@@ -28,21 +28,34 @@ NotebookFooter(;case=nothing, sfid=nothing, pims=nothing) = @htl("""
         <a href="https://tuftsctsi.lightning.force.com/lightning/r/Case/$sfid/view">$case</a>
         """))
       <br />"""))
-    $(isnothing(pims) ? "" : @htl("""PIMS #
-        <a href="https://pims.tuftsctsi.org/issues/$(pims)">$(pims)</a>"""))
     </td></tr></table>
   </div>
 """)
 
-NotebookHeader(title=nothing, subtitle=nothing; case=nothing, sfid=nothing) = @htl("""
-   <h1>$(title)
-   </h1>
-  $(isnothing(case) ? "" : @htl("""
-    <div style="text-align: center;">Service Request#
-    $(isnothing(sfid) ? @htl("""<span>$case</span>""") : @htl("""
-      <a href="https://tuftsctsi.my.site.com/s/case/$sfid/">$case</a>
-      """))
-    </div>"""))
-   $(isnothing(subtitle) ? "" :
-     @htl("""<p style="text-align: left; font-style: italic; font-size: 24px;">$subtitle</p>"""))
+NotebookHeader(title=nothing, subtitle=nothing; case=nothing, sfid=nothing, irb=nothing) = @htl("""
+   <div style="overflow: auto; width: 100%; vertical-align: top;">
+     <h1 style="display: inline-block; width: 88%; text-align: left; vertical-align: top;">
+       $(title)
+       $(isnothing(subtitle) ? "" :
+         @htl("""<p style="font-style: italic; font-size: 21px;">$subtitle</p>"""))
+     </h1>
+     <div style="display: inline-block; width: 11%; text-align: right;
+                 height: 100%; vertical-align: middle;">
+       $(isnothing(case) ? "" : @htl("""Service Request#
+          $(isnothing(sfid) ? @htl("""<span>$case</span>""") : @htl("""
+            <a style="text-decoration: underline dotted;"
+               href="https://tuftsctsi.my.site.com/s/case/$sfid/">$case</a>
+        """))"""))
+    </div>
+   </div>
+   $(if isnothing(irb) || irb == 11642
+        @htl("""
+            <p>
+                This cohort discovery is provided under IRB Protocol #11642,
+                <i>"Accelerating Clinical Trials - Multi-institutional cohort discovery"</i>,
+                permitting <i>"aggregate, obfuscated patient counts"</i>.
+                Counts below ten are indicated with the ≤ symbol.
+            </p>
+        """)
+     end)
 """)
