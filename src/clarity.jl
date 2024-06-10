df690a2b573026b79fb1815ce8e67b0a0e3adbe6
Clark C. Evans <cce@clarkevans.com>
2024-06-10 05:53:36 -0400

    adding define_do_not_contact() which should be moved to ETL

--- src/clarity.jl
+++ src/clarity.jl
@@ -31,3 +31,14 @@ end
 
 funsql_decode_c(name::Symbol, map::Pair...; default=nothing) =
     decode_nest!(name, collect(map), something(default, name))
+
+# TODO: move to ETL
+@funsql define_do_not_contact() = begin
+    join(person=>person(), person.person_id == person_id)
+    left_join(do_not_contact => from(`patient_4`).filter(RSH_PREF_C == 2),
+        person.omop.person_source_value == do_not_contact.PAT_ID)
+    define(do_not_contact => isnotnull(do_not_contact.PAT_ID))
+    with(`patient_4` =>
+        from($(FunSQL.SQLTable(qualifiers = [:main, :epicclarity], name = :patient_4,
+                               columns = [:PAT_ID, :RSH_PREF_C]))))
+end
