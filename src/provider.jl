9e55b71573c091d95f8f1d6840cd7a4040cf797f
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2024-12-23 18:52:47 -0500

    Remove provider()

--- src/provider.jl
+++ src/provider.jl
@@ -1,32 +1,5 @@
 @funsql begin
 
-provider() = begin
-    from(provider)
-    define(is_preepic => provider_id > 1000000000)
-    as(omop)
-    define(
-        provider_id => omop.provider_id,
-        provider_name => omop.provider_name,
-        npi => omop.npi,
-        dea => omop.dea,
-        concept_id => coalesce(omop.specialty_concept_id, 0),
-        care_site_id => omop.care_site_id,
-        year_of_birth => omop.year_of_birth,
-        gender_concept_id => coalesce(omop.gender_concept_id, 0))
-    left_join(
-        concept => concept(),
-        concept_id == concept.concept_id,
-        optional = true)
-    left_join(
-        care_site => care_site(),
-        care_site_id == care_site.care_site_id,
-        optional = true)
-    left_join(
-        gender_concept => concept(),
-        concept_id == gender_concept.concept_id,
-        optional = true)
-end
-
 provider(match...) =
     provider().filter(concept_matches($match))
 
