7ff5633e36549333353c04b2a01224d2bf13f172
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2024-12-23 18:48:00 -0500

    Remove concept()

--- src/concept.jl
+++ src/concept.jl
@@ -1,19 +1,5 @@
 @funsql begin
 
-concept() = begin
-    from(concept)
-    as(omop)
-    define(
-        omop.concept_id,
-        omop.concept_name,
-        omop.domain_id,
-        omop.vocabulary_id,
-        omop.concept_class_id,
-        omop.standard_concept,
-        omop.concept_code,
-        omop.invalid_reason)
-end
-
 concept(concept_id::Integer...) =
     concept().filter(in(concept_id, $concept_id...))
 
