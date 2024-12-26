0bbd432629b7be56be02b2dee945cb36faaba6bb
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2024-12-26 10:08:46 -0500

    Migrate provider dependency

--- src/vocabulary.jl
+++ src/vocabulary.jl
@@ -343,16 +343,6 @@ function funsql_concept_sets_breakout(pair::Pair{Symbol, NamedConceptSets}; with
     end
 end
 
-# TODO: remove backward compatibility
-funsql_concept_matches(cs; on = :concept_id) =
-    funsql_isa(on, cs)
-
-funsql_concept_matches(cs::Tuple{Any}; on = :concept_id) =
-    funsql_concept_matches(cs[1], on = on)
-
-funsql_concept_matches(cs::Vector; on = :concept_id) =
-    funsql_concept_matches(FunSQL.Append(args = FunSQL.SQLNode[cs...]), on = on)
-
 function print_concepts(df; prefix="        ")
     df = DataFrame(df)
     first = true
