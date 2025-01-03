7d959314b4913b02dc8a9e7be7b372e7bb49d412
Ashlin Harris <90787010+AshlinHarris@users.noreply.github.com>
2025-01-02 21:18:00 -0500

    Update vocabulary.jl

--- src/vocabulary.jl
+++ src/vocabulary.jl
@@ -279,39 +279,6 @@ end
 funsql_isa_strict(concept_id, concept_set::AbstractVector) =
     @funsql in($concept_id, append(args=$concept_set).select(concept_id))
 
-function funsql_isa(concept_id, concept_set)
-    concept_set = convert(FunSQL.SQLNode, concept_set)
-    concept_set = @funsql($concept_set.concept_descendants())
-    return @funsql $concept_id in $concept_set.select(concept_id)
-end
-
-funsql_isa(concept_id, concept_set::AbstractVector) =
-    @funsql isa($concept_id, append(args=$concept_set))
-
-function funsql_isa_icd(concept_id, concept_set; with_icd9gem=false)
-    concept_set = convert(FunSQL.SQLNode, concept_set)
-    concept_set = @funsql($concept_set.concept_icd_descendants())
-    if with_icd9gem
-        concept_set = @funsql begin
-            append(
-                $concept_set,
-                $concept_set.concept_relatives("ICD10CM - ICD9CM rev gem"))
-       end
-    end
-    return @funsql $concept_id in $concept_set.select(concept_id)
-end
-
-funsql_isa_icd(concept_id, concept_set::AbstractVector; with_icd9gem=false) =
-   @funsql isa_icd($concept_id, append(args=$concept_set); with_icd9gem=$with_icd9gem)
-
-@funsql isa(concept_set; with_icd9gem=false) = begin
-     isa(concept_id, $concept_set) ||
-     if_defined_scalar(icd_concept,
-        isa_icd(icd_concept.concept_id, $concept_set;
-                with_icd9gem=$with_icd9gem),
-        false)
-end
-
 function funsql_define_isa(ncs::NamedConceptSets; with_icd9gem=false)
     query = @funsql(define())
     for (name, cset) in pairs(ncs)
