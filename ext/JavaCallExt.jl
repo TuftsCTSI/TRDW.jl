70f736708297daa5113f7d8afe07e05d1b7ead7a
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2026-09-11 18:42:42 -0400

    Display JavaCall error details

--- ext/JavaCallExt.jl
+++ ext/JavaCallExt.jl
@@ -131,6 +131,14 @@ function _write_cell!(
         end
     catch e
         @error "Failed to write cell (sheet=$(sheet_name), column=$(col_sym), row=$(row_idx))" exception=(e,)
+        if e isa JavaCall.JavaError
+            @error "JavaCall error details" classname=JavaCall.jclassname(JavaCall.jexception(e)) jmsg=JavaCall.jerrmsg(e)
+            try
+                JavaCall.jcall(JavaCall.jexception(e), "printStackTrace", Nothing, ())
+            catch
+                # ignore secondary errors while printing stack trace
+            end
+        end
         rethrow()
     end
 end
